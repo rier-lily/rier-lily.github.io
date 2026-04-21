921a9f225d9695daedfb32a600aeb1e07a4cb5ef
rier-lily <rier@pdx.edu>
2026-04-21 05:27:03 +0000

    work

--- guide-to-climate-inequality/scripts/scripts.js
+++ guide-to-climate-inequality/scripts/scripts.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', event => {
   const sections3 = gsap.utils.toArray('#side3 section');
 
   // Horizontal Scroll 1
-  gsap.to(sections1, {
+  const scroll1 = gsap.to(sections1, {
     xPercent: -100 * (sections1.length - 1),
     ease: 'none',
     scrollTrigger: {
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', event => {
   });
 
   // Horizontal Scroll 3
-  gsap.to(sections3, {
+  const scroll3 = gsap.to(sections3, {
     xPercent: -100 * (sections3.length - 1),
     ease: 'none',
     scrollTrigger: {
@@ -40,14 +40,46 @@ document.addEventListener('DOMContentLoaded', event => {
       end: '+=3000',
     },
   });
-});
 
-gsap.to(".your-element", {
-  filter: "hue-rotate(180deg)",
-  scrollTrigger: {
-    trigger: "#side2",
-    start: "top top",
-    end: "bottom bottom",
-    scrub: true // Links the hue rotation directly to the scrollbar position
-  }
+  // Hue rotation tied to vertical scroll
+  gsap.to(".bg-img", {
+    filter: "hue-rotate(0deg)",
+    scrollTrigger: {
+      trigger: "#side1",
+      start: "top top",
+      end: "#side2 top top",
+      scrub: true
+    }
+  });
+
+  gsap.to(".bg-img", {
+    filter: "hue-rotate(-74deg)",
+    scrollTrigger: {
+      trigger: "#c2",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: true
+    }
+  });
+
+  // Hue rotation tied to horizontal scroll 3
+  gsap.to(".bg-img", {
+    filter: "hue-rotate(-207deg)",
+    scrollTrigger: {
+      trigger: "#c3",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: true
+    }
+  });
+
+  gsap.to(".bg-img", {
+    filter: "hue-rotate(-74deg)",
+    scrollTrigger: {
+      trigger: "#end",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: true
+    }
+  });
 });
